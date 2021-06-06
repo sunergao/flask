b2d5bc97d08eae9244f3e4a4d43331d717d7c722
sunergao <1213872426@qq.com>
2021-06-06 09:25:10 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask import escape,url_for
+from flask import escape,url_for,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '主页'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/home/<name>')
 def user_page(name):
@@ -17,3 +17,11 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'test_page'
+
+name = 'sunergao'
+movies = [
+    {'title':'大话西游','year':'1999'},
+    {'title':'肖申克的救赎','year':'1999'},
+    {'title':'动物也疯狂','year':'1999'},
+    {'title':'楚门的世界','year':'1999'},
+]
